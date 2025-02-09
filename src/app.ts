cae68689d806e232119dae3e8dc2770fba010267
srxrt <str9703@gmail.com>
2025-02-09 14:28:00 +0900

    feat: develop socket.io server

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,14 @@ import session from "express-session";
 import ConnectMongoDBSession from "connect-mongodb-session";
 import { T } from "./libs/types/common";
 import cookieParser from "cookie-parser";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
 
 const MongoDBStore = ConnectMongoDBSession(session);
 
 const store = new MongoDBStore({
-	uri: String(process.env.MONGO_URL),
-	collection: "sessions",
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
 });
 
 // entrance
@@ -30,21 +32,21 @@ app.use(morgan(MORGAN_FORMAT));
 
 // sessions
 app.use(
-	session({
-		secret: String(process.env.SESSION_SECRET),
-		cookie: {
-			maxAge: 1000 * 3600 * 6, // 6h
-		},
-		store: store,
-		resave: true,
-		saveUninitialized: true,
-	})
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 6, // 6h
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  }),
 );
 
 app.use((req, res, next) => {
-	const sessionInstance = req.session as T;
-	res.locals.member = sessionInstance.member;
-	next();
+  const sessionInstance = req.session as T;
+  res.locals.member = sessionInstance.member;
+  next();
 });
 
 // views
@@ -54,4 +56,22 @@ app.set("view engine", "ejs");
 //Routers
 app.use("/", router);
 app.use("/admin", routerAdmin); //EJS
-export default app; // module.exports = app;
+
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: { origin: true, credentials: true },
+});
+
+let summaryClient = 0;
+io.on("connection", (socket) => {
+  summaryClient++;
+  console.log(`connection && total ${summaryClient}`);
+
+  socket.on("disconnect", () => {
+    summaryClient--;
+
+    console.log(`disconnect && total ${summaryClient}`);
+  });
+});
+
+export default server; // module.exports = app;
