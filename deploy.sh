2f6b2a9977594cdd4537336a857c2095cb30384e
srxrt <str9703@gmail.com>
2025-02-14 02:03:20 +0900

    fix: modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
+nvm use 16.17.0 || {
+  echo "Failed to use Node.js version"
+  exit 1
+}
 #PRODUCTION
 git reset --hard
 git checkout master
-git pull origin master
+pwd
+git pull origin master || {
+  echo "Git pull failed"
+  exit 1
+}
 
 npm i
 npm run build
