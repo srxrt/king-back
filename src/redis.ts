315c6f926ffb54b77730027fdda095d29feeff20
srxrt <str9703@gmail.com>
2025-03-25 13:44:29 +0900

    feat: integrate redis

---
+++ src/redis.ts
@@ -0,0 +1,12 @@
+import Redis from "ioredis";
+
+const redis = new Redis();
+
+redis.on("connect", () => {
+  console.log("Connected to Redis!");
+});
+redis.on("error", (err) => {
+  console.log("Redis error:", err);
+});
+
+export default redis;
